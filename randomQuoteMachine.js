85d1ff901d5438cf2495b3ad827ba1be4a64f51f
Al Yablonsky <ablonsky@austin.rr.com>
2017-03-06 19:44:17 -0600

    Add files via upload

--- randomQuoteMachine.js
+++ randomQuoteMachine.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
 
-    var randomQuote, randomAuthor;
-   
-    // Obtaining a random quote and author through the forismatic API
+  
+	randomQuoteMachineNameSpace = function(){
+	    var randomQuote, randomAuthor;
+	    function getRandomQuote(){return randomQuote;}
+	    function getRandomAuthor(){return randomAuthor;}
+	    function setRandomQuote(quoteValue){randomQuote = quoteValue;}
+	    function setRandomAuthor(authorValue){randomAuthor =authorValue;}
+	    return {
+		    getRandomQuote:getRandomQuote,
+		    getRandomAuthor:getRandomAuthor,
+		    setRandomQuote:setRandomQuote,
+		    setRandomAuthor:setRandomAuthor
+	    }
+	}();
+	
     
+	
     function getQuote() {
-    
-/*        
-    var url="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";  // Version using JSON
-    $.getJSON(url,function(data) {
-        $("#quote").html('"' + data.quoteText + '"'); 
-        $("#author").html("-" + data.quoteAuthor);  
-        randomQuote = data.quoteText;
-        randomAuthor = data.quoteAuthor;
-        });
-        
-*/
-        
-/* Beginning of work    */
-        
+   			
         // Version using ajax obtaining random quote and author through the Forismatic app via URL
         $.ajax({                                        
             url: "http://api.forismatic.com/api/1.0/",
@@ -31,31 +31,33 @@ $(document).ready(function () {
             },
             
             success: function(response) {
-                randomQuote = response.quoteText;     // Obtaining the quote
-                randomAuthor = response.quoteAuthor;   // Obtaining the author
-                $("#quote").text(randomQuote);
-                if (randomAuthor) {
-                    $("#author").html("- " + randomAuthor);
+                var randomQuote1 = response.quoteText;     
+                var randomAuthor1 = response.quoteAuthor;
+                randomQuoteMachineNameSpace.setRandomQuote(randomQuote1);
+                randomQuoteMachineNameSpace.setRandomAuthor(randomAuthor1);
+				
+                $("#quote").text(randomQuote1);
+                if (randomAuthor1) {
+                    $("#author").html("- " + randomAuthor1);
                 } else {
-                    $("#author").html("- unknown");     // Case unkown Author
+                    $("#author").html("- unknown");     
                 }   
             }            
-        }); // end of ajax
+        }); 
          
-/*    End of work  */        
-        
-        
-    };  // End of get quote
-    
-    // Opening a box for tweeting the quote and its author "on Click"
+    };  
     
     $("#tweet").on("click",function(event) {
+		
+		 var randomQuote2 = randomQuoteMachineNameSpace.getRandomQuote();
+		 var randomAuthor2 = randomQuoteMachineNameSpace.getRandomAuthor();
+		
         event.preventDefault();
-            if (randomAuthor) {
-                window.open("https://twitter.com/intent/tweet?text=" + randomQuote + " - " + randomAuthor );
+            if (randomAuthor2) {
+                window.open("https://twitter.com/intent/tweet?text=" + randomQuote2 + " - " + randomAuthor2 );
             }
             else {
-                window.open("https://twitter.com/intent/tweet?text=" + randomQuote + " -unknown ");
+                window.open("https://twitter.com/intent/tweet?text=" + randomQuote2 + " -unknown ");
             }
     });  
     
@@ -64,30 +66,28 @@ $(document).ready(function () {
         event.preventDefault();
         getQuote();
         
-        // Array of dark colors for random coloring of the area surrounding the quote and its author
+       
         var colorArray = ["darkblue", "cadetblue",  "crimson", "brown",  "darkturquoise", "fuchsia", "hotpink",  "navy", "purple",  "teal",  "silver",  "red",  "forestgreen",  "slategray", "coral", "deeppink", "firebrick", "darkkhaki", "forestgreen", "maroon", "darkslategray"];
         var  colorIndex = colorArray.length;
         
-        // Random color generated from colors of the color array, these are dark colors so the quote and author are easy to see
-        var colorValue = Math.floor((Math.random() * colorIndex) + 1);
-        var hue = colorArray[colorValue];
         
-        // Random color generated from 0-255 r,g,b and a (transparency fixed at 0.6 ), this is used to mix with the image in the "surroundings" div
-        var hue1 = 'rgba(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + 0.65 + ')';
+        var colorValue = Math.floor((Math.random() * colorIndex) + 1);
+        var randomColorOfQuote = colorArray[colorValue];
+        
+       
+        var randomColorOfSurroundings = 'rgba(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + 0.65 + ')';
         
         // Setting font color of quote and author expression, background color of the body, "newquote" and "tweet" button
-        document.body.style.backgroundColor = hue;
-        document.getElementById("quote").style.color = hue;
-        document.getElementById("author").style.color = hue;
+        document.body.style.backgroundColor = randomColorOfQuote;
+        document.getElementById("quote").style.color = randomColorOfQuote;
+        document.getElementById("author").style.color = randomColorOfQuote;
     
         
-        // Using Jquery to style background colors and colors for all button elements
-            $(".btn").css({"background-color" : hue });
+       
+            $(".btn").css({"background-color" : randomColorOfQuote});
 
-        // surroundindgs fileld background color set to very random value
-        document.getElementById("surroundings").style.backgroundColor = hue1;
         
-    
+        document.getElementById("surroundings").style.backgroundColor = randomColorOfSurroundings;
         
-    });  // end of "new quote - OnClick" 
-});  // End of $document ready 
+    });   
+});  
